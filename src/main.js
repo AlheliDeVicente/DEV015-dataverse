c06b1677e31b7dac356a6508533fed8bfb1348a1
Alheli De Vicente <devicentealheli@gmail.com>
2024-06-20 12:34:10 -0600

    i don't even know anymore

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import { renderItems } from "./view.js";
 import data from "./data/dataset.js";
 
 //obtener radioButtons, los selectores de filtros, el boton de limpar y el de mostrar la data//
-const branchSelector = document.querySelector('[data-testid="select-sort"]');
-const radioButtons = document.getElementsByName("sort-order");
-const typeSelector = document.querySelector('[data-testid="select-sort1"]');
+const branchSelector = document.querySelector('[data-testid="select-filter"]');
+const orderSelector = document.querySelector('[data-testid="select-sort"]');
+const typeSelector = document.querySelector('[data-testid="select-filter1"]');
 const cleanButton = document.querySelector('[data-testid="button-clear"]');
 const statsButton = document.querySelector('[data-testid="show-stats"]');
 const statsSection = document.getElementById("stats");
-const classSelector = document.querySelector('[data-testid="select-sort2"]');
+const classSelector = document.querySelector('[data-testid="select-filter2"]');
 const rootElement = document.getElementById('root');
 // se obtiene renderItems//
 const ulElement = renderItems(data);
@@ -49,20 +49,14 @@ classSelector.addEventListener("change", () => {
 });
 
 //fución para ordenar, se le añade un event listener a cada uno de los radiobuttons//
-radioButtons.forEach(function (radio) {
-  radio.addEventListener("change", () => {
-    //evento change registrar cambios registra cambios en la selección//
-    const selectedRadio = document.querySelector(
-      'input[name="sort-order"]:checked'
-    ); //se obtiene cuál es el radio button seleccionado en ese momento//
-    const selectedOrder = selectedRadio.value; //se obtiene el valor del radio button seleccionado//
-    const sortedData = sortData(filteredData, "century", selectedOrder); //la constante sortered data obtiene el valor de filtered data pasada por la funci+on sort data//
-    rootElement.innerHTML=""
-    const ulElement = renderItems(sortedData);
-    rootElement.appendChild(ulElement)
-  });
+orderSelector.addEventListener("change", () => {
+  //evento change registra cambios en la selección//
+  const orderSelected = orderSelector.value;
+  const sortedData = sortData(filteredData, "century", orderSelected);
+  rootElement.innerHTML = ""
+  const ulElement=renderItems(sortedData);
+  rootElement.append(ulElement)
 });
-
 //boton de limpiar//
 cleanButton.addEventListener("click", () => {
   typeSelector.value = ""; //se le da el valor "" a los filtros//
@@ -80,8 +74,8 @@ statsButton.addEventListener("click", () => {
   const womenPhilosophers = womenPhilosophersStats(data)
   const philosophersBeforeXIX = philosophersBeforeXIXStats(data);
   const analyticPhilosopher = analyticPhilosopherStats(data)
-  statsParagraphs.innerHTML = `<p> Hay un promedio de ${womenPhilosophers}% mujeres filosofas</p>  
+  statsParagraphs.innerHTML = `<p> Hay un promedio de ${womenPhilosophers} mujeres filosofas</p>  
   <p> Alrededor de ${philosophersBeforeXIX}% de los filósofos son anteriores al siglo XIX</p>
-  <p> Hay un promedio de ${analyticPhilosopher}% filósofos analítcos `;
+  <p> Hay un promedio de ${analyticPhilosopher} filósofos analítcos `;
   statsSection.appendChild(statsParagraphs); // Añadir el p al cuerpo del documento
 });
